7f127710adfb6dbed0757bf0ac89ffd267d2e92f
Manjunath-Bhashyam <manjunathbhashyam1995@gmail.com>
2024-08-04 13:53:33 +0530

    updated utils/common.py

--- src/DeepClassifier/utils/common.py
+++ src/DeepClassifier/utils/common.py
@@ -28,6 +28,7 @@ def read_yaml_file(path_to_yaml: Path) -> ConfigBox:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
