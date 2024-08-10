1c2b22177b2df0e402363e0d9d5c12845ee3f3d9
Manjunath-Bhashyam <manjunathbhashyam1995@gmail.com>
2024-08-10 12:04:20 +0530

    Updates in requirements affected files

--- setup.py
+++ setup.py
@@ -6,26 +6,10 @@ with open("README.md","r",encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "CNN-Classifier"
+REPO_NAME = "CNN_Classifier"
 AUTHOR_USER_NAME = "Manjunath-Bhashyam"
 SRC_REPO = "deepclassifier"
 AUTHOR_EMAIL = "manjunathbhashyam1995@gmail.com"
-REQUIREMENT_FILE_NAME = "requirements.txt"
-HYPHEN_E_DOT = "-e ."
-
-def get_requirements_list() -> List[str]:
-    """
-    Description: This function is used to return requirements list mention in requirements.txt file
-    returns: This function is going to return a list which contains the names of libraries
-    mentioned in requirements.txt file
-    """
-
-    with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()
-        requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
-        if HYPHEN_E_DOT in requirement_list:
-            requirement_list.remove(HYPHEN_E_DOT)
-        return requirement_list
 
 setuptools.setup(
     name=SRC_REPO,
@@ -38,6 +22,5 @@ setuptools.setup(
     description="Python Package for CNN app",
     package_dir={"":"src"},
     packages=setuptools.find_packages(where="src"),
-    project_urls = {"Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"},
-    install_requires=get_requirements_list()
-)
+    project_urls = {"Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"}
+    )
