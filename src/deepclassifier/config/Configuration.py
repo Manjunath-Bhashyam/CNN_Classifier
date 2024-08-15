3f103e48266811347026a86fb92cd0694e76066b
Manjunath-Bhashyam <manjunathbhashyam1995@gmail.com>
2024-08-15 14:17:09 +0530

    stage2 added to src

--- src/deepclassifier/config/Configuration.py
+++ src/deepclassifier/config/Configuration.py
@@ -1,6 +1,7 @@
 from deepclassifier.utils import read_yaml_file, create_directories
 from deepclassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
-from deepclassifier.entity import DataIngestionConfig
+from deepclassifier.entity import DataIngestionConfig,PrepareBaseModelConfig
+from pathlib import Path
 
 
 class ConfigurationManager:
@@ -20,3 +21,20 @@ class ConfigurationManager:
                                                     unzipped_dir=config.unzipped_dir)
 
         return data_ingestion_config
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(root_dir=Path(config.root_dir),
+                                                    base_model_path=Path(config.base_model_path),
+                                                    updated_base_model_path=Path(config.updated_base_model_path),
+                                                    params_image_size=self.params.IMAGE_SIZE,
+                                                    params_include_top=self.params.INCLUDE_TOP,
+                                                    params_learning_rate=self.params.LEARNING_RATE,
+                                                    params_weights=self.params.WEIGHTS,
+                                                    params_classes=self.params.CLASSES
+                                                            )
+        
+        return prepare_base_model_config
